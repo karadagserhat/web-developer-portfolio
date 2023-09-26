5f545f1a38cacec56d4cde4ed308dbbe6be8d0d0
karadagserhat <serhatkaradagg@outlook.com>
2023-09-26 15:47:22 +0300

    change footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,24 @@ const Footer = () => {
     <footer className="bg-tertiary py-12">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 items-center justify-between">
+          {/* logo */}
+          <div className="text-2xl font-signature text-white lg:mr-[-5rem]">
+            Serhat Karadağ
+          </div>
+
+          {/* copyright */}
+          <p className="lg:mr-[-6rem]">
+            &copy; {new Date().getFullYear()} Serhat Karadağ. All rights
+            reserved.
+          </p>
+
           {/*social icons  */}
           <div className="flex space-x-6 items-center justify-center">
             {social.map((item, index) => {
               const { href, icon } = item;
               return (
                 <a
-                  className="text-accent text-base"
+                  className="text-accent text-lg"
                   target="_blank"
                   // rel="noreferrer"
                   rel="noreferrer"
@@ -24,17 +35,6 @@ const Footer = () => {
               );
             })}
           </div>
-
-          {/* logo */}
-          <div className="text-2xl font-signature text-white lg:mr-[-5rem]">
-            Serhat Karadağ
-          </div>
-
-          {/* copyright */}
-          <p>
-            &copy; {new Date().getFullYear()} Serhat Karadağ. All rights
-            reserved.
-          </p>
         </div>
       </div>
     </footer>
